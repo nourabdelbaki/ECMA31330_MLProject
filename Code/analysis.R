c18a04f34818274676b19720b3409f6e6a72d3a7
nourabdelbaki <nourabdelbaki@uchicago.edu>
2025-03-13 22:26:04 -0500

    Cross Validated RandomForests

--- Code/analysis.R
+++ Code/analysis.R
@@ -53,13 +53,37 @@ hyper_grid <- expand.grid(
   rmse = NA                                              
 )
 
+### TAKES AROUND 10 MINS TO RUN!! 
+for(i in seq_len(nrow(hyper_grid))) {
+  # fit model for ith hyperparameter combination
+  fit <- randomForest(formula= US ~ ., data = data, ntree = p * 10, 
+                      mtry = hyper_grid$mtry[i], 
+                      nodesize = hyper_grid$nodesize[i],
+                      replace = hyper_grid$replace[i],
+                      sampsize = hyper_grid$sampsize[i]
+  )
+  # save rmse
+  hyper_grid$rmse[i] <- sqrt(fit$mse)
+}
 
+hyper_grid_all <- hyper_grid %>%
+  arrange(rmse)
 
+head(hyper_grid_all, 10)
+#     mtry nodesize replace sampsize      rmse
+#1    80       15   FALSE      259    0.3479939
+#2    66       15    TRUE      259    0.4057546
+#3    30        3    TRUE      162    0.4061437
+#4    30        3   FALSE      259    0.4272611
 ############################################################################
 #  Random Forests- Entire Dataset
 ###########################################################################
 rf_model <- randomForest(US ~ ., data = data, importance = TRUE,
-                         ntree = 2000, replace = TRUE, 
+                         ntree = p*10,
+                         mtry = 80,
+                         nodesize = 15,
+                         replace = FALSE,
+                         sampsize = 80,
                          keep.forest = TRUE)
 
 # Extract feature importance
@@ -116,7 +140,7 @@ effect_JP <- partial_deriv_approx(rf_model, data_predict, "JP",
                                   delta_value)
 
 ### Method 2: SHAP breakdown (not really betas but rather game theoretic contribution)
-explainer <- explain(rf_model, data = data, #data w/o date column 
+explainer <- explain(rf_model, data = data,
                      y = data$US, label = "Random Forest")
 
 # SHAP values to interpret feature importance at a local level
@@ -131,25 +155,108 @@ plot(shap_values)
 # Ref: https://cran.r-project.org/web/packages/pdp/pdp.pdf 
 # train is the dataset we used for training the rfmodel
 # computation takes a long time, so suggested maximum 3 pred.var() for the partial()
+## Takes around 10 mins to run as well:
 
 # Show marginal effect of each monetary policy, holding the rest fixed:
-pdp_ca <- partial(rf_model, pred.var = c("CA", "lag_GB", "FR"), train = data) 
-plotPartial(pdp_ca, 
+pdp_rf_all <- partial(rf_model, pred.var = c("CA", "lag_GB", "FR"), train = data) 
+plotPartial(pdp_rf_all, 
       main = "Partial Dependence of US Monetary Policy on Foreign Monetary policy")
 
 # Individual impact; not very intuitive, but:
-pdp_gb <- partial(rf_model, pred.var = "lag_GB", train = data) 
-plotPartial(pdp_gb, 
+pdp_gb_rf_all <- partial(rf_model, pred.var = "lag_GB", train = data) 
+plotPartial(pdp_gb_rf_all, 
             main = "Partial Dependence of US Monetary Policy on GB Monetary policy")
 ############################################################################
+#  Random Forests- Before and After 2008- Cross Validation 
+###########################################################################
+# Datasets:
+# Officially the Great Recession/2008 Financial Crisis lasted from Dec 2007
+# to June 2009. We chose the after '08 dataset to also end before the 
+# COVID-19 crisis. 
+datab4_08 <- data[data$date < as.yearmon("Dec 2007", "%b %Y"),] 
+dataAfter08 <- data[data$date >=  as.yearmon("Dec 2007", "%b %Y") &
+                      data$date < as.yearmon("March 2020", "%b %Y"), ]
+
+#### Cross-validation before 2008:
+n_08 <- dim(datab4_08)[1]
+
+hyper_grid_b4_08 <- expand.grid(
+  mtry = floor(p * c(.05, .15, .25, .333, .4)),
+  nodesize = c(3, 5, 10, 15),
+  replace = c(TRUE, FALSE),                            
+  sampsize = floor(n_08* c(.5, .63, .8)),             
+  rmse = NA                                              
+)
+
+### TAKES AROUND 4 MINS TO RUN:
+for(i in seq_len(nrow(hyper_grid_b4_08))) {
+  # fit model for ith hyperparameter combination
+  fit <- randomForest(formula= US ~ ., data = datab4_08, ntree = p * 10, 
+                      mtry = hyper_grid_b4_08$mtry[i], 
+                      nodesize = hyper_grid_b4_08$nodesize[i],
+                      replace = hyper_grid_b4_08$replace[i],
+                      sampsize = hyper_grid_b4_08$sampsize[i]
+  )
+  # save rmse
+  hyper_grid_b4_08$rmse[i] <- sqrt(fit$mse)
+}
+
+hyper_grid_b4_08 <- hyper_grid_b4_08 %>%
+  arrange(rmse)
+
+head(hyper_grid_b4_08, 10)
+#     mtry nodesize replace sampsize      rmse
+#1    66        5   FALSE       95    0.2325632
+#2    80       15   FALSE       74    0.2668251
+#3    80        3   FALSE       95    0.3233085
+#4    80       15    TRUE       95    0.3263221
+#5    50        5   FALSE       95    0.3424027
+
+#### Cross-validation after 2008:
+n_08_2 <- dim(dataAfter08)[1]
+
+hyper_grid_after_08 <- expand.grid(
+  mtry = floor(p * c(.05, .15, .25, .333, .4)),
+  nodesize = c(3, 5, 10, 15),
+  replace = c(TRUE, FALSE),                            
+  sampsize = floor(n_08_2* c(.5, .63, .8)),             
+  rmse = NA                                              
+)
+
+### TAKES AROUND 6 MINS TO RUN:
+for(i in seq_len(nrow(hyper_grid_after_08))) {
+  # fit model for ith hyperparameter combination
+  fit <- randomForest(formula= US ~ ., data = dataAfter08, ntree = p * 10, 
+                      mtry = hyper_grid_after_08$mtry[i], 
+                      nodesize = hyper_grid_after_08$nodesize[i],
+                      replace = hyper_grid_after_08$replace[i],
+                      sampsize = hyper_grid_after_08$sampsize[i]
+  )
+  # save rmse
+  hyper_grid_after_08$rmse[i] <- sqrt(fit$mse)
+}
+
+hyper_grid_after_08 <- hyper_grid_after_08 %>%
+  arrange(rmse)
+
+head(hyper_grid_after_08, 10)
+#     mtry nodesize replace sampsize      rmse
+#1    30        5   FALSE      117    0.1070436
+#2    66       10   FALSE       73    0.1078672
+#3    10        5   FALSE      117    0.1894131
+#4    50       10    TRUE      117    0.1940878
+#5    50        5   FALSE       92    0.2224923
+###########################################################################
 #  Random Forests- Before and After 2008 
 ###########################################################################
-datab4_08 <- data[data$date < as.yearmon("Jan 2008", "%b %Y"),]
-dataAfter08 <- data[!data$date < as.yearmon("Jan 2008", "%b %Y"),]
-
+####### Random Forest Before '08 
 RFmodel_b4_08 <- randomForest(US ~ ., data = datab4_08, importance = TRUE,
-                         ntree = 1000, replace = TRUE, 
-                         keep.forest = TRUE)
+                              ntree = p*10,
+                              mtry = 66,
+                              nodesize = 5,
+                              replace = FALSE,
+                              sampsize = 95,
+                              keep.forest = TRUE)
 
 # Extract feature importance
 importance_plot2 <- importance(RFmodel_b4_08)
@@ -170,8 +277,13 @@ barplot(top_50_2$Importance, names.arg = top_50_2$Feature,
         main = "Top 50 Feature Importance Before 2008", 
         cex.names = 0.7) # Reduce text size for readability
 
+####### Random Forest After '08 
 RFmodel_after_08 <- randomForest(US ~ ., data = dataAfter08, importance = TRUE,
-                                 ntree = 1000, replace = TRUE, 
+                                 ntree = p*10,
+                                 mtry = 30,
+                                 nodesize = 5,
+                                 replace = FALSE,
+                                 sampsize = 117,
                                  keep.forest = TRUE)
 
 # Extract feature importance
@@ -192,6 +304,8 @@ barplot(top_50_3$Importance, names.arg = top_50_3$Feature,
         las = 2, col = "steelblue",
         main = "Top 50 Feature Importance After 2008", 
         cex.names = 0.7) # Reduce text size for readability
+
+### Merged Plot: 
 
 # Merge both sets to align matching features
 merged_importance_08 <- merge(top_50_3, top_50_2, by = "Feature",
@@ -217,8 +331,8 @@ long_importance_08 <- merged_importance_08 %>%
 ggplot(long_importance_08, aes(x = reorder(Feature, Importance), 
                             y = Importance, fill = Dataset)) +
   geom_bar(stat = "identity", position = position_dodge()) +
-  scale_fill_manual(values = c("After 2008" = "steelblue", 
-                               "Before 2008" = "darkorange")) +
+  scale_fill_manual(values = c("Before 2008" = "darkorange",
+                               "After 2008" = "steelblue")) +
   labs(title = "Comparison of Feature Importance Before and After 2008",
        x = "Feature",
        y = "Importance",
@@ -253,16 +367,100 @@ effect_EU_after_08 <- partial_deriv_approx(rf_model, data_pred_after_08, "FR",
                                         delta_value)
 effect_JP_after_08 <- partial_deriv_approx(rf_model, data_pred_after_08, "JP", 
                                         delta_value)
+
+############################################################################
+#  Random Forests- Before and After COVID-19- Cross Validation 
+###########################################################################
+# Datasets:
+# Officially the Great Recession/2008 Financial Crisis lasted from Dec 2007
+# to June 2009. We chose the before covid dataset to be from after June 2009 
+# to before March 2020. And after to be from March 2020 onwards. 
+datab4_covid <- data[data$date >= as.yearmon("June 2009", "%b %Y") &
+                       data$date < as.yearmon("March 2020", "%b %Y"),] 
+dataAfterCovid <- data[data$date >=  as.yearmon("March 2020", "%b %Y"), ]
+
+#### Cross-validation before COVID-19:
+n_covid <- dim(datab4_covid)[1]
+
+hyper_grid_b4_covid <- expand.grid(
+  mtry = floor(p * c(.05, .15, .25, .333, .4)),
+  nodesize = c(3, 5, 10, 15),
+  replace = c(TRUE, FALSE),                            
+  sampsize = floor(n_covid* c(.5, .63, .8)),             
+  rmse = NA                                              
+)
+
+### TAKES AROUND 5 MINS TO RUN:
+for(i in seq_len(nrow(hyper_grid_b4_covid))) {
+  # fit model for ith hyperparameter combination
+  fit <- randomForest(formula= US ~ ., data = datab4_covid, ntree = p * 10, 
+                      mtry = hyper_grid_b4_covid$mtry[i], 
+                      nodesize = hyper_grid_b4_covid$nodesize[i],
+                      replace = hyper_grid_b4_covid$replace[i],
+                      sampsize = hyper_grid_b4_covid$sampsize[i]
+  )
+  # save rmse
+  hyper_grid_b4_covid$rmse[i] <- sqrt(fit$mse)
+}
+
+hyper_grid_b4_covid <- hyper_grid_b4_covid %>%
+  arrange(rmse)
+
+head(hyper_grid_b4_covid, 10)
+#     mtry nodesize replace sampsize      rmse
+#1    10        5   FALSE      103      0.02451452
+#2    80        5   FALSE      103      0.08492078
+#3    66        5    TRUE       64      0.09494432
+#4    66        5   FALSE       81      0.09547033
+#5    80       10   FALSE      103      0.10071857
+
+#### Cross-validation after COVID-19:
+n_covid_2 <- dim(dataAfterCovid)[1]
+
+hyper_grid_after_covid <- expand.grid(
+  mtry = floor(p * c(.05, .15, .25, .333, .4)),
+  nodesize = c(3, 5, 10, 15),
+  replace = c(TRUE, FALSE),                            
+  sampsize = floor(n_covid_2* c(.5, .63, .8)),             
+  rmse = NA                                              
+)
+
+### TAKES AROUND 2 MINS TO RUN:
+for(i in seq_len(nrow(hyper_grid_after_covid))) {
+  # fit model for ith hyperparameter combination
+  fit <- randomForest(formula= US ~ ., data = dataAfterCovid, ntree = p * 10, 
+                      mtry = hyper_grid_after_covid$mtry[i], 
+                      nodesize = hyper_grid_after_covid$nodesize[i],
+                      replace = hyper_grid_after_covid$replace[i],
+                      sampsize = hyper_grid_after_covid$sampsize[i]
+  )
+  # save rmse
+  hyper_grid_after_covid$rmse[i] <- sqrt(fit$mse)
+}
+
+hyper_grid_after_covid <- hyper_grid_after_covid %>%
+  arrange(rmse)
+
+head(hyper_grid_after_covid, 10)
+#     mtry nodesize replace sampsize      rmse
+#1    80        5   FALSE       46    0.05705443
+#2    10       15   FALSE       36    0.13846380
+#3    80        3   FALSE       36    0.17699976
+#4    10        3   FALSE       46    0.20728905
+#5    66       15   FALSE       46    0.20956426
 ############################################################################
 #  Random Forests- Before and After COVID-19
 ###########################################################################
-datab4_covid <- data[data$date < as.yearmon("March 2020", "%b %Y"),]
-dataAfterCovid <- data[!data$date < as.yearmon("March 2020", "%b %Y"),]
-
-
-RFmodel_b4_covid <- randomForest(US ~ ., data = datab4_covid, importance = TRUE,
-                                 ntree = 1000, replace = TRUE, 
+####### Random Forest Before COVID-19
+RFmodel_b4_covid <- randomForest(US ~ .,
+                                 data = datab4_covid, importance = TRUE,
+                                 ntree = p*10,
+                                 mtry = 10,
+                                 nodesize = 5,
+                                 replace = FALSE,
+                                 sampsize = 103,
                                  keep.forest = TRUE)
+
 # Extract feature importance
 importance_plot4<- importance(RFmodel_b4_covid)
 
@@ -283,9 +481,13 @@ barplot(top_50_4$Importance, names.arg = top_50_5$Feature,
         cex.names = 0.7) # Reduce text size for readability
 
 
-RFmodel_after_covid <- randomForest(US ~ ., data = dataAfterCovid,
-                                    importance = TRUE,
-                                    ntree = 1000, replace = TRUE, 
+RFmodel_after_covid <- randomForest(US ~ .,
+                                    data = dataAfterCovid, importance = TRUE,
+                                    ntree = p*10,
+                                    mtry = 80,
+                                    nodesize = 5,
+                                    replace = FALSE,
+                                    sampsize = 46,
                                     keep.forest = TRUE)
 
 # Extract feature importance
@@ -307,7 +509,7 @@ barplot(top_50_5$Importance, names.arg = top_50_5$Feature,
         main = "Top 50 Feature Importance After COVID-19", 
         cex.names = 0.7) # Reduce text size for readability
 
-####
+#### Merged Plot: 
 # Plotting both together for a more intuitive feel of what's happening before
 # and after a crisis:
 
@@ -335,8 +537,8 @@ long_importance <- merged_importance_covid %>%
 ggplot(long_importance, aes(x = reorder(Feature, Importance), 
                             y = Importance, fill = Dataset)) +
   geom_bar(stat = "identity", position = position_dodge()) +
-  scale_fill_manual(values = c("After COVID-19" = "steelblue", 
-                               "Before COVID-19" = "darkorange")) +
+  scale_fill_manual(values = c("Before COVID-19" = "darkorange",
+                               "After COVID-19" = "steelblue")) +
   labs(title = "Comparison of Feature Importance Before and After COVID-19",
        x = "Feature",
        y = "Importance",
