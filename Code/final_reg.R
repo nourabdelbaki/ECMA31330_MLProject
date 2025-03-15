da60bbe071dc9b5cff4b9c8ed9ae44b17abda7e6
gtriberti9 <giulytriberti@gmail.com>
2025-03-15 06:43:14 -0500

    Created final code

--- Code/final_reg.R
+++ Code/final_reg.R
@@ -1,5 +1,7 @@
 # Nour Abdelbaki and Giuliana Triberti
 
+library(zoo)
+
 # USING DATA "1998_G7_US"
 data <- read.csv("1998_G7_US.csv", na.strings = "NA")
 
@@ -14,6 +16,8 @@ data <- read.csv("1998_G7_US.csv", na.strings = "NA")
 # different cluster to different regions.
 
 inf_clust <- read.csv("inflation_clustered.csv", na.strings = "NA")
+inf_clust <- inf_clust[,-1]
+inf_clust$date <- data$date
 mon_clust <- read.csv("monetary_clustered.csv", na.strings = "NA")
 
 
@@ -22,8 +26,25 @@ mon_clust <- read.csv("monetary_clustered.csv", na.strings = "NA")
 # the number of variables to test the MSE and define how the tree will move forward.
 # We take the betas from this approach.
 
+data_08_all <- read.csv("data_08_all.csv", na.strings = "NA")
 
+data_covid_all <- read.csv("data_covid_all.csv", na.strings = "NA")
 
+df_08 <- left_join(data_08_all, inf_clust, by = "date")
+df_08<- df_08[,-1] # Drop duplicate row index 
+
+df_covid <- left_join(data_covid_all, inf_clust, by = "date")
+df_covid <- df_covid[,-1]
+
+# Motivated by our monetary policy clustering using K-means, we group the entire
+# EU together by averaging their interest rates. Note that this has an impact
+# only before June 1998 and transition time for Italy's rate to settle at the EU
+# rate. 
+df_08 <- df_08 %>%
+  mutate(EU = rowMeans(select(., DE, FR, IT)))
+
+df_covid <- df_covid %>%
+  mutate(EU = rowMeans(select(., DE, FR, IT)))
 
 # Finally, we will run a regression to see if a difference in the coefficient of 
 # a variable on the US has to do with a crisis period or not. In this regression
@@ -33,14 +54,61 @@ mon_clust <- read.csv("monetary_clustered.csv", na.strings = "NA")
 
 
 # Run second-stage regression
-second_stage <- lm(Beta ~ t + factor(inf_clust) + factor(mon_clust) + 
-                     factor(Cluster_unemployment), 
-                   data = second_stage_data)
+second_stage <- lm(beta_CA ~ crisis + Clus_inf_1 + Clus_inf_2
+                   + rgdp + rgdp_growth + UNRATE + EXPINF1YR + lag_GB + EU +
+                     JP + VIXCLS + WTISPLC + GSCPI, 
+                   data = df_08)
+
+second_stage2 <- lm(beta_GB ~ crisis + Clus_inf_1 + Clus_inf_2
+                   + rgdp + rgdp_growth + UNRATE + EXPINF1YR + CA + EU +
+                     JP + VIXCLS + WTISPLC + GSCPI, 
+                   data = df_08)
+
+summary(second_stage2)
+
+second_stage2 <- lm(beta_EU ~ crisis + Clus_inf_1 + Clus_inf_2
+                    + rgdp + rgdp_growth + UNRATE + EXPINF1YR + CA + lag_GB +
+                      JP + VIXCLS + WTISPLC + GSCPI, 
+                    data = df_08)
+
+
+# Run second-stage regression
+second_stage <- lm(beta_CA ~ crisis + Clus_inf_1 + Clus_inf_2
+                   + rgdp + rgdp_growth + UNRATE + EXPINF1YR + lag_GB + EU +
+                     JP + VIXCLS + WTISPLC + GSCPI, 
+                   data = df_covid)
 
 
 
+second_stage2 <- lm(beta_GB ~ crisis + Clus_inf_1 + Clus_inf_2
+                    + rgdp + rgdp_growth + UNRATE + EXPINF1YR + CA + EU +
+                      JP + VIXCLS + WTISPLC + GSCPI, 
+                    data = df_covid)
 
 
+second_stage2 <- lm(beta_FR ~ crisis + Clus_inf_1 + Clus_inf_2
+                    + rgdp + rgdp_growth + UNRATE + EXPINF1YR + CA + lag_GB +
+                      JP + VIXCLS + WTISPLC + GSCPI, 
+                    data = df_covid)
+
+
+########
+set.seed(123)
+df_08$randomPlacebo <- sample(df_08$crisis)
+
+# We need to format the date for future use
+df_08 <- df_08 %>%
+  mutate(date = seq.Date(from = as.Date("1998-01-01"), by = "month", length.out = n()))
+
+df_08$ShiftCrisis <- ifelse(df_08$date < "2006-01-01",
+                            0, 1)
+
+second_stage <- lm(beta_CA ~ ShiftCrisis + Clus_inf_1 + Clus_inf_2
+                   + rgdp + rgdp_growth + UNRATE + EXPINF1YR + lag_GB + EU +
+                     JP + VIXCLS + WTISPLC + GSCPI, 
+                   data = df_08)
+
+summary(second_stage)
 
 
 
